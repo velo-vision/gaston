285d5fd277dba6b4e65c75a5cbba6d18b5af4891
jorge <jorge.f@velosoft.mx>
2017-03-21 10:41:27 -0600

    Se sube articulos jorge

--- wp-content/themes/gaston2/articulos.php
+++ wp-content/themes/gaston2/articulos.php
@@ -13,12 +13,12 @@ get_header();
     <div class="col l10 offset-l1 m12 s12 margin-50">
       <div class="col l3 m6 s12">
         <div class="col l12 m12 s12 select-caballo-div nomargin" style="margin-top:5px;">
-           <?php wp_dropdown_categories( $args ); ?> 
+           <?php wp_dropdown_categories( $args ); ?>
         </div>
       </div>
       <div class="col l4 m6 s12">
         <div class="col l12 m12 s12 select-caballo-div" style="margin-top:5px;">
-           <?php wp_dropdown_users( $args ); ?> 
+           <?php wp_dropdown_users( $args ); ?>
         </div>
       </div>
       <div class="col l5 m8 offset-m2 s12" style="margin-top:15px;">
@@ -45,52 +45,39 @@ get_header();
       </div>
     </div>
     <div class="col l12 m12 s12 margin-30">
-<?php
-  $args = array( 'numberposts' => '3' );
-  $recent_posts = wp_get_recent_posts( $args );
-  foreach( $recent_posts as $recent ){?>
-<!-- contenido -->
+     <?php
+    global $post;
+    $args = array( "posts_per_page" => 3, "offset"=> 0, "category" => 12 );
+    $myposts = get_posts( $args );
+    foreach( $myposts as $post ) : setup_postdata($post); ?>
     <div class="col l4 m6 s12" style="margin-bottom:20px;">
       <div class="col l12 m12 s12 size relative nopadding">
         <a href="<?php the_permalink(); ?>"></a>
         <div class="imggastonblog">
-      <?php $video = get_post_meta($recent["ID"], 'video1', true); //video1 es un campo personalizado, con custom fields
-       if (!empty($video)){                
-           echo "<iframe class='video33' height='500' src='https://www.youtube.com/embed/".$video."' frameborder='0' allowfullscreen></iframe>";
-          }elseif (!empty(has_post_thumbnail($recent["ID"]))){
-                     $url = wp_get_attachment_url( get_post_thumbnail_id($recent["ID"]) );
-                    echo '<li class="img_articulos_bg" style="background: url('. $url.')">'; 
-                  }
-        else{
-            echo "no hay imagen o video disponible";
-          }
-           ?>     
+          <?php
+          if (has_post_thumbnail()) {
+            $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+            echo '<li class="img_articulos_bg" style="background: url('. $url.')">';}?>
         </div>
         <div class="col s12 m12 l12 nopadding nomargin">
-          <div class="col l5 m8 s5 text-titulo nopadding">
-          <?php echo single_cat_title("", true); ?>
-            <?php  //echo $recent["post_name"]; ?> 
+        	<div class="col l5 m8 s5 text-titulo nopadding">
+            <?php echo the_category() ?>
           </div>
           <div class="col l7 m4 s7 text-fecha nopadding">
             <?php echo get_the_date( get_option('date_format') ); ?>
           </div>
-        </div>          
+        </div>
           <div class="col l12 m12 s12 text-contenido">
-            <?php  echo $recent["post_title"]; ?>  
+            <?php the_title(); ?>
           </div>
           <div class="col l12 m12 s8 offset-s2 text-boton margin-30">
             <div class="col 12 m12 s8 titulo_articulos_home">
-              <a href="<?php the_permalink($recent["ID"]); ?>">Leer más <i class="material-icons">arrow_forward</i></a>
+              <a href="<?php the_permalink(); ?>">Leer más <i class="material-icons">arrow_forward</i></a>
             </div>
           </div>
-        </div>      
-    </div>    
-<!-- conetinido -->
-     <?php
-     //var_dump($url);
-  }
-  wp_reset_query();
-?>
+        </div>
+    </div>
+    <?php endforeach; ?>
 </div>
 <div class="row nopadding">
   <div class="col s12 m12 l10 offset-l1 margin-70 nopadding">
@@ -105,95 +92,83 @@ get_header();
     </div>
   </div>
 </div>
-<div class="row"><!--mas leidas--> 
-<?php
-query_posts('posts_per_page=3&meta_key=post_views_count&orderby=meta_value_num&order=DESC');
-if (have_posts()) : while (have_posts()) : the_post();
-?>
-    <div class="col l4 m6 s12" style="margin-bottom:20px;">
-      <div class="col l12 m12 s12 size relative nopadding">
-        <a href="<?php the_permalink(); ?>"></a>
-        <div class="imggastonblog">
-          <!-- <?php  
-          if (has_post_thumbnail()) {
-            $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
-            echo '<li class="img_articulos_bg" style="background: url('. $url.')">';}?>    --> 
+<div class="row"><!--mas leidas-->
+<div class="col l12 m12 s12 articulos_gaston bgarticulos_home slider-articulos size-articulos">
+  <div class="slidercinco">
+    <?php
+    global $post;
+    $args = array( "posts_per_page" => 3, "offset"=> 0, "category" => 12 );
+    $myposts = get_posts( $args );
+    foreach( $myposts as $post ) : setup_postdata($post); ?>
+    <div class="slide background-opacity">
 
-        <?php $video = get_post_meta($post->ID, 'video1', true); //video1 es un campo personalizado, con custom fields
-       if (!empty($video)){                
-           echo "<iframe class='video33' height='500' src='https://www.youtube.com/embed/".$video."' frameborder='0' allowfullscreen></iframe>";
-          }elseif (!empty(has_post_thumbnail())){
-                    $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );                    
-                   //echo '<img class="fondo-pagevideo2" src="'. $url.'">'; 
-                   echo '<li class="img_articulos_bg" style="background: url('. $url.')">'; 
-                  }
-        else{
-            echo "no hay imagen o video disponible";
-          }
-           ?>       
+      <a href="<?php the_permalink(); ?>"></a>
+      <div class="col l12 m12 s12  imggastonblog">
+        <?php
+        if (has_post_thumbnail()) {
+          $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+          echo '<li class="img_articulos_bg" style="background: url('. $url.')">';
+        }
+        ?>
+      </div>
+      <div class="col l5 m5 s5 text-titulo nopadding">
+        <?php echo the_category() ?>
+      </div>
+      <div class="col l7 m7 s7 text-fecha nopadding">
+        <?php echo get_the_date( get_option('date_format') ); ?>
+      </div>
+      <div class="col l12 m12 s12 text-contenido">
+        <?php the_title(); ?>
+      </div>
+      <div class="col l12 m12 s8 offset-s2 text-boton margin-30">
+        <div class="col 12 m12 s8 titulo_articulos_home">
+          <a href="<?php the_permalink(); ?>">Leer más <i class="material-icons ">arrow_forward</i></a>
         </div>
-        <div class="col s12 m12 l12 nopadding nomargin">
-          <div class="col l5 m8 s5 text-titulo nopadding">
-            <?php echo the_category() ?>
-          </div>
-          <div class="col l7 m4 s7 text-fecha nopadding">
-            <?php echo get_the_date( get_option('date_format') ); ?>
-          </div>
-        </div>          
-          <div class="col l12 m12 s12 text-contenido">
-            <?php the_title(); ?>    
-          </div>
-          <div class="col l12 m12 s8 offset-s2 text-boton margin-30">
-            <div class="col 12 m12 s8 titulo_articulos_home">
-              <a href="<?php the_permalink(); ?>">Leer más <i class="material-icons">arrow_forward</i></a>
-            </div>
-          </div>
-        </div>      
+      </div>
     </div>
-<?php
-endwhile; endif;
-wp_reset_query();
-?>
-</div><!-- fin mas leidas-->
+  <?php endforeach; ?>
+</div>
+</div>
+   <!-- Botones
+   <div class="col l12 m12 s12 center articulos margin-50 slider-articulos">
+      <p> Más articulos</p>
+    </div> Fin Botones -->
+</div><!--mas leidas-->
 <div class="row nopadding">
   <div class="col s12 m12 l10 offset-l1 margin-30 nopadding">
     <div class="col s3 m4 l4 div-tam center-align nopadding">
       <div class="linea"></div>
     </div>
     <div class="col s6 m4 l4 div-tam center-align borde-articulos nopadding">
-     VIDEOS
+      MÁS ARTÍCULOS
     </div>
     <div class="col s3 m4 l4 div-tam center-align nopadding">
       <div class="linea"></div>
     </div>
   </div>
 </div>
+
 <div class="col s12 m12 l12 masarticuls">
-  <!-- <div class="slidermasarticulos "> -->
-    <div class="col s12 m12 l12"> 
+  <div class="slidermasarticulos ">
+    <div class="col s12 m12 l12">
+
+
+
+
       <?php
       global $post;
      // $rand =mt_rand(11,12);
-      $args = array("posts_per_page" => 3, "offset"=> 0, "category" =>  19 ); //categoria videos
+      $args = array(/* "posts_per_page" => 3,*/ "offset"=> 0, "category" =>  11 );
       $myposts = get_posts( $args );
       foreach( $myposts as $post ) : setup_postdata($post); ?>
-      <!-- <div class="slide">     -->
-        <div class="col l4 m4 s12" style="margin-bottom:20px;">      
+      <div class="slide">
+        <div class="col l4 m4 s12" style="margin-bottom:20px;">
           <div class="col l12 m12 s12 size relative fondo-blanco">
             <a href="<?php the_permalink(); ?>">
               <div class="imggastonblog">
-              <?php $video = get_post_meta($post->ID, 'video1', true);
-                   if (!empty($video)){                
-                       echo "<iframe class='video33' height='554' src='https://www.youtube.com/embed/".$video."' frameborder='0' allowfullscreen></iframe>";
-                      }elseif (!empty(has_post_thumbnail())){
-                                $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );                    
-                               //echo '<img class="fondo-pagevideo2" src="'. $url.'">'; 
-                               echo '<li class="img_articulos_bg" style="background: url('. $url.')">'; 
-                              }
-                    else{
-                        echo "no hay imagen o video disponible";
-                      }
-                       ?>  
+                <?php if (has_post_thumbnail()) {
+                  $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+                  echo '<li class="img_articulos_bg" style="background: url('. $url.')">'; } ?>
                 </div></a>
                 <div class="col l5 m7 s5 text-titulo ">
                   <?php echo the_category() ?>
@@ -202,7 +177,7 @@ wp_reset_query();
                   <?php echo get_the_date( get_option('date_format') ); ?>
                 </div>
                 <div class="col l12 m12 s12 text-contenido">
-                  <?php the_title(); ?>    
+                  <?php the_title(); ?>
                 </div>
                 <div class="col l12 m12 s8 offset-s2 text-boton margin-30">
                   <div class="col 12 m12 s8 titulo_articulos_home">
@@ -210,12 +185,69 @@ wp_reset_query();
                   </div>
                 </div>
               </div>
-            </div> 
-          <!-- </div> -->
-        <?php endforeach; ?> 
+            </div>
+          </div>
+        <?php endforeach; ?>
+
       </div>
-   <!--  </div> -->
+    </div>
   </div>
+<!---articulos-->
+<!-- Botones --><!--
+<div class="row">
+  <div class="col l12 m12 s12 articulos margin-50">
+    <div class="col l12 m12 s12 center">
+      <p>
+        <i class="material-icons">keyboard_backspace</i> Más articulos <i class="material-icons">arrow_forward</i>
+      </p>
+    </div>
+  </div>
+</div>-->
+<!-- Fin Botones -->
+<!--
+<div class="row hide-on-small-only">
+  <div class="col s12 m12 l10 offset-l1 margin-30">
+    <div class="col s4 m4 l4 div-tam center-align">
+      <div class="linea"></div>
+    </div>
+    <div class="col s4 m4 l4 div-tam center-align borde-articulos">
+      AUTORES
+    </div>
+    <div class="col s4 m4 l4 div-tam center-align">
+      <div class="linea"></div>
+    </div>
+  </div>
+</div>
+<div class="col l10 offset-l1 m12 s12 center margin-50 lista-caras autores-slider hide-on-small-only">-->
+<!-- autores-->
+<!--
+<div class="sliderautoresmin">
+<?php /*for ($x=1; $x <=15 ; $x++) { ?>
+  <?php for ($i=1; $i <=5; $i++) { ?>
+   <div class="slide">
+      <div class="col l12 m12 s12 center-align">
+        <div class="col l10 offset-l1">
+          <img src="<?php bloginfo('template_url'); ?>/images/articulos/circulo<?php echo $i; ?>.png" alt="user" width="100" height="100">
+        </div>
+        <p>lorem ipsum</p>
+      </div>
+    </div>
+  <?php } ?>
+  <?php }*/ ?>
+  </div>
+</div>-->
+<!-- Botones -->
+<!--
+<div class="row hide-on-small-only">
+  <div class="col l12 m12 s12 articulos margin-50">
+    <div class="col l12 m12 s12 center">
+      <p>
+        <i class="material-icons">keyboard_backspace</i> Más articulos <i class="material-icons">arrow_forward</i>
+      </p>
+    </div>
+  </div>
+</div>-->
+<!-- Fin Botones -->
 </div>
 
 <?php
