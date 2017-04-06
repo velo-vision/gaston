889a9a920f743630b2b76246d64560c46cc0adb9
jorge <jorge.f@velosoft.mx>
2017-04-06 13:38:13 -0500

    agregue algo en el archive

--- wp-content/themes/gaston2/archive.php
+++ wp-content/themes/gaston2/archive.php
@@ -3,19 +3,19 @@
 Template Name: Archive
 */
 get_header(); ?>
-<div class="row">  
+<div class="row">
     <?php if (have_posts()) : while (have_posts()) : the_post();  ?>
         <div class="col l12 m12 s12 historial" style="margin-top:50px;">
             <a href="<?php the_permalink(); ?>" style="color:#000;" >
-                <div class="col s12 m12 l6 imagenfija">        
+                <div class="col s12 m12 l6 imagenfija">
                     <?php $video = get_post_meta($post->ID, 'video1', true);
-                    if (!empty($video)){                
+                    if (!empty($video)){
                         echo "<iframe class='video2' height='554' src='https://www.youtube.com/embed/".$video."' frameborder='0' allowfullscreen></iframe>";
                     }else{
                         if (has_post_thumbnail()){
-                            $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );                    
-//echo "<img src='". $url."' class='img_articulos_bg' />";
-                            echo '<li class="fondo-pagevideo2" style="background: url('. $url.')">';   
+                            $url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+//echo ok "<img src='". $url."' class='img_articulos_bg' />";
+                            echo '<li class="fondo-pagevideo2" style="background: url('. $url.')">';
                         }
                     } ?>
                 </div></a>
@@ -37,7 +37,7 @@ get_header(); ?>
                                     <?php echo get_the_date( get_option('date_format') ); ?>
                                 </p>
                             </div>
-                        </div>                        
+                        </div>
                         <div class="col l12 m12 s12 text-historial">
                             <p>
                                 <?php echo get_the_excerpt( get_the_ID() ) ?>
@@ -45,7 +45,7 @@ get_header(); ?>
                         </div>
                         <div class="col l6 m6 s6 text-compartir">
                             <span class='st_facebook_large' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
-                            <span class='st_twitter_large' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span> 
+                            <span class='st_twitter_large' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>'></span>
                         </div>
                     </div>
                 </div>
@@ -64,4 +64,3 @@ get_header(); ?>
     <?php endif; ?>
 </div>
 <?php //get_footer(); ?>
-
